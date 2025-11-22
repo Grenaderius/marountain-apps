957dbbc9dd394771150fe6349a71b3593ff57055
Grenaderius <nazarkorbutak@gmail.com>
2025-11-22 12:51:00 +0000

    Comments controller changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,32 @@
 class CommentsController < ApplicationController
-  # GET /comments
+  skip_before_action :verify_authenticity_token
+
   def index
     comments = Comment.includes(:user)
-    render json: comments, include: {
-      user: { only: [:id, :name] }
-    }
+    render json: comments, include: { user: { only: [:id, :name, :email] } }
   end
 
-  # POST /comments
   def create
     comment = Comment.new(comment_params)
+
     if comment.save
-      render json: comment, include: {
-        user: { only: [:id, :name] }
-      }, status: :created
+      render json: comment, include: { user: { only: [:id, :name, :email] } }, status: :created
     else
       render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  # DELETE /comments/:id
+  def destroy
+    comment = Comment.find(params[:id])
+
+    # авторизація: юзер може видаляти тільки свій коментар
+    if comment.user_id != params[:user_id].to_i
+      return render json: { error: "Not allowed" }, status: :forbidden
+    end
+
+    comment.destroy
+    render json: { message: "Comment deleted" }
   end
 
   private
