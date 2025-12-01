f98103701fa7664bbda885c4ba4825854f58d5da
Grenaderius <nazarkorbutak@gmail.com>
2025-12-01 11:12:12 +0000

    Application controller fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,61 +1,24 @@
-class AppsController < ApplicationController
-  before_action :authorize_request, only: [:create, :update, :destroy]
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
 
-  def index
-    apps = App.all.includes(:comments)
-    result = apps.map do |app|
-      {
-        id: app.id,
-        name: app.name,
-        is_game: app.is_game,
-        photo: app.photo_path,
-        rating: app.comments.any? ? app.comments.average(:rating).to_f.round(1) : 0,
-        dev_id: app.dev_id
-      }
-    end
-    render json: result
-  end
-
-  def show
-    app = App.includes(comments: :user).find(params[:id])
-    render json: app, include: {
-      comments: { include: { user: { only: [:id, :email, :name] } } }
-    }
-  end
-
-  def create
-    app = App.new(app_params.merge(dev_id: @current_user.id))
-    if app.save
-      render json: app, status: :created
-    else
-      render json: { errors: app.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    app = App.find(params[:id])
-    return render json: { error: "Not allowed" }, status: :forbidden unless app.dev_id == @current_user.id
-
-    if app.update(app_params)
-      render json: app
-    else
-      render json: { errors: app.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    app = App.find(params[:id])
-    return render json: { error: "Not allowed" }, status: :forbidden unless app.dev_id == @current_user.id
-    app.destroy
-    head :no_content
-  end
+  before_action :authorize_request
 
   private
 
-  def app_params
-    params.require(:app).permit(
-      :name, :description, :is_game, :cost, :size, 
-      :android_min_version, :ram_needed, :photo_path, :apk_path
-    )
+  def authorize_request
+    @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
+
+    if request.headers['Authorization'].present?
+      token = request.headers['Authorization'].split(' ').last
+      decoded = JsonWebToken.decode(token) rescue nil
+      @current_user ||= User.find_by(id: decoded[:user_id]) if decoded
+    end
+
+    unless @current_user
+      respond_to do |format|
+        format.html { redirect_to "/login", alert: "Please log in first." }
+        format.json { render json: { error: "Unauthorized" }, status: :unauthorized }
+      end
+    end
   end
 end
