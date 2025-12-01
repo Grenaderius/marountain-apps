6fa97d189b8a86213a7d32acd26c291cb195e14a
Grenaderius <nazarkorbutak@gmail.com>
2025-12-01 11:28:07 +0000

    Application controller fix 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,20 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
-
+class ApplicationController < ActionController::API
   before_action :authorize_request
 
   private
 
   def authorize_request
-    @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
+    @current_user = nil
 
-    # JWT autorize
+    # JWT авторизація
     if request.headers['Authorization'].present?
       token = request.headers['Authorization'].split(' ').last
       decoded = JsonWebToken.decode(token) rescue nil
-      @current_user ||= User.find_by(id: decoded[:user_id]) if decoded
+      @current_user = User.find_by(id: decoded[:user_id]) if decoded
     end
 
     return if @current_user
 
-    respond_to do |format|
-      format.json { render json: { error: "Unauthorized" }, status: :unauthorized }
-      format.html { redirect_to "/login" } # Для HTML запитів просто редірект без flash
-    end
+    render json: { error: "Unauthorized" }, status: :unauthorized
   end
 end
