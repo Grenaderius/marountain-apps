6ac400e44e81be37da354f90a18ed22a5a59b622
Grenaderius <nazarkorbutak@gmail.com>
2025-12-01 11:17:48 +0000

    Application controller fix 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,17 +8,18 @@ class ApplicationController < ActionController::Base
   def authorize_request
     @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
 
+    # JWT autorize
     if request.headers['Authorization'].present?
       token = request.headers['Authorization'].split(' ').last
       decoded = JsonWebToken.decode(token) rescue nil
       @current_user ||= User.find_by(id: decoded[:user_id]) if decoded
     end
 
-    unless @current_user
-      respond_to do |format|
-        format.html { redirect_to "/login", alert: "Please log in first." }
-        format.json { render json: { error: "Unauthorized" }, status: :unauthorized }
-      end
+    return if @current_user
+
+    respond_to do |format|
+      format.json { render json: { error: "Unauthorized" }, status: :unauthorized }
+      format.html { redirect_to "/login" } # Для HTML запитів просто редірект без flash
     end
   end
 end
