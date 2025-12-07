8a907f3dc70815e77392b90d770c9e1120909164
Grenaderius <nazarkorbutak@gmail.com>
2025-12-07 22:22:20 +0000

    minor text error fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true,
-                    format: { with: URI::MailTo::EMAIL_REGEXP, message: "повинна бути дійсною email-адресою" }
+                    format: { with: URI::MailTo::EMAIL_REGEXP, message: " needs to be active" }
   validates :password, presence: true, length: { minimum: 6 }
 end
