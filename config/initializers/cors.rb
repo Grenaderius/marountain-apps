db378fd1ed075ab4ec57e5f334e886e2a4cdc375
Grenaderius <nazarkorbutak@gmail.com>
2025-12-07 17:07:14 +0000

    Add changing apps info fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://myapp-frontend.vercel.app", "http://localhost:5173"
+    origins "https://marountain-apps-2nd-a5ezbx2xo-nazars-projects-ed8fd5f8.vercel.app", "http://localhost:5173"
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
