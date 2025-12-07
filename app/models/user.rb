db8fb262b37634f5759546064159014f85746455
Grenaderius <nazarkorbutak@gmail.com>
2025-12-07 16:07:25 +0000

    Add apps delet

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: URI::MailTo::EMAIL_REGEXP, message: "повинна бути дійсною email-адресою" }
   validates :password, presence: true, length: { minimum: 6 }
 end
