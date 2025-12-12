ca3e645f302f86f092f11aa618e1ffe7fa1348a9
Grenaderius <nazarkorbutak@gmail.com>
2025-12-12 12:30:56 +0000

    Urls fix for payment sys

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -30,8 +30,8 @@ class PaymentsController < ApplicationController
         },
         quantity: 1
       }],
-      success_url: "#{ENV['DOMAIN']}/payment-success?session_id={CHECKOUT_SESSION_ID}",
-      cancel_url: "#{ENV['DOMAIN']}/payment-cancel"
+      success_url: "#{ENV['DOMAIN']}/games",
+      cancel_url: "#{ENV['DOMAIN']}/games"
     )
 
     render json: { url: session.url }
