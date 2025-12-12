94fea73b59fb6e343f7348b5e07be3365e13daa2
Grenaderius <nazarkorbutak@gmail.com>
2025-12-12 16:40:29 +0000

    fix db payments 4

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,5 +1,5 @@
 class WebhooksController < ApplicationController
-  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
 
   def stripe
     payload = request.body.read
