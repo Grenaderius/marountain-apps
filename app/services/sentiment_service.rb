3b83ae9761b7eeaba9baaf6612963234e9f77a67
Grenaderius <nazarkorbutak@gmail.com>
2025-12-14 11:58:49 +0000

    comments fix 9

--- app/services/sentiment_service.rb
+++ app/services/sentiment_service.rb
@@ -2,7 +2,7 @@ require "net/http"
 require "json"
 
 class SentimentService
-  API_URL = "https://router.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english"
+  API_URL = "https://router.huggingface.co/v1/inference/models/distilbert-base-uncased-finetuned-sst-2-english"
   CONFIDENCE_THRESHOLD = 0.55
 
   def self.analyze(text)
@@ -13,21 +13,16 @@ class SentimentService
     request = Net::HTTP::Post.new(uri)
     request["Authorization"] = "Bearer #{ENV['HF_TOKEN']}"
     request["Content-Type"] = "application/json"
+    request["Accept"] = "application/json"
+    request["User-Agent"] = "Rails-Sentiment-Service"
     request.body = { inputs: text }.to_json
 
     response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
       http.request(request)
     end
 
-    content_type = response["content-type"].to_s
-
     unless response.is_a?(Net::HTTPSuccess)
       Rails.logger.error("HF HTTP error #{response.code}: #{response.body}")
-      return "NEUTRAL"
-    end
-
-    unless content_type.include?("application/json")
-      Rails.logger.error("HF non-JSON response: #{response.body}")
       return "NEUTRAL"
     end
 
